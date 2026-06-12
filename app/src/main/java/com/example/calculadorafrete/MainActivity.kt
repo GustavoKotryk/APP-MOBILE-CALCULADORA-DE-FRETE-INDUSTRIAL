3c831b77d1f3ec241aed5e9a8cec7fbdb0dc4b97
GUSTAVO RAFAEL KOTRYK <gustavo_kotryk@estudante.sesisenai.org.br>
2026-06-12 15:20:28 -0300

    feat: MainActivity

--- app/src/main/java/com/example/calculadorafrete/MainActivity.kt
+++ app/src/main/java/com/example/calculadorafrete/MainActivity.kt
@@ -18,5 +18,27 @@ class MainActivity : AppCompatActivity() {
         val textViewPercentual = findViewById<TextView>(R.id.textViewPercentual)
         val textViewFrete = findViewById<TextView>(R.id.textViewFrete)
         val textViewTotal = findViewById<TextView>(R.id.textViewTotal)
+
+        buttonCalcular.setOnClickListener {
+            val regiao = editTextRegiao.text.toString()
+            val valor = editTextValor.text.toString().toFloat()
+
+            val percentual = when (regiao.uppercase()){
+                "SUL" -> 0.05f
+                "SUDESTE" -> 0.07f
+                "CENTRO-OESTE" -> 0.08f
+                "NORDESTE" -> 0.10f
+                "NORTE" -> 0.12f
+                else -> 0.0f
+            }
+
+            val valorFrete = valor * percentual
+            val valorTotal = valor + valorFrete
+
+            textViewPercentual.text="Percentual: ${(percentual * 100).toInt()}%"
+            textViewFrete.text = "Valor do frete: R$ %.2f".format(valorFrete)
+            textViewTotal.text = "Valor total: R$ %.2f".format(valorTotal)
+
+        }
     }
 }
